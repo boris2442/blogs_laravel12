ba59ed4f2e16a474749e9ae80139f3f90adcee43
boris2442 <aubinborissimotsebo@gmail.com>
2025-07-22 15:56:09 +0200

    organisation du code

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,22 +1,6 @@
-{{-- @dd($posts) --}}
-
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {{-- <title>{{ config('app.name') }}</title> --}}
-    <title>
-        {{-- {{ @yield('title')}} --}}
-        accueil du blog
-    </title>
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body class="antialiased pt-10 pb-16 md:pb-32">
+@extends('layouts.admin.layout-admin')
+@section('title', 'Accueil du blog')
+@section('content')
     {{-- Conteneur global --}}
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         {{-- Header --}}
@@ -132,6 +116,4 @@
             </div>
         </main>
     </div>
-</body>
-
-</html>
+@endsection
