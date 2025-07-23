a9c8c0ae45ba291be1ac3613c305a8fe168c923a
boris2442 <aubinborissimotsebo@gmail.com>
2025-07-23 13:30:29 +0200

    install breeze and begin comment

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/', [PostController::class, 'index'])->name('posts.index');
+use App\Http\Controllers\ProfileController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/category/{category}', [PostController::class, 'postsByCategory'])->name('posts.category');
 Route::get('/posts/tag/{tag}', [PostController::class, 'postsByTag'])->name('posts.tag');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+require __DIR__.'/auth.php';
